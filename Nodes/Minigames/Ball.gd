7d53a0e0eb774faf23c122472daf03a7b9133d61
GamesFromANobody <gamesfromanobody@gmail.com>
2024-01-17 22:35:21 +0100

    Pong minigame update + added Pong to GameEngine

---
+++ Nodes/Minigames/Ball.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+@export var Basespeed = Vector2(100.0, 10.0)
+@export var speedMultiplier = 1
+@export var Difficulty = 1
+@export var cooldownTime = 30
+
+var speed 
+
+func _ready():
+	velocity = Vector2(1, 1)
+	speed = Basespeed
+
+func _physics_process(delta):
+	
+	var collision = move_and_collide(velocity * speed * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+		if (collision.get_normal().y == 0):
+			speed = randomizeY()
+		print(collision.get_normal())
+
+
+func randomizeY():
+	return Basespeed * speedMultiplier * Vector2(1, randf_range(3, 6) * (randi_range(0, 1) * 2 - 1)) 
+	
+
+
+func _on_death_border_body_entered(body):
+	print(body.name)
+	if (body.name == name):
+		print("deth")
+	pass # Replace with function body.
